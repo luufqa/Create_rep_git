e7ed2ff23c5e11185df778455aafb2c835f2d7e5
Alexey Pshenitsyn <luufqa@gmail.com>
2024-01-22 00:22:01 +0300

    Fix v2.0

--- data/user_data.py
+++ data/user_data.py
@@ -1,5 +1,5 @@
 class User:
     user_email = "your email" # add email
-    user_password = "your password" # add pass
+    user_password = "your pass" # add pass
     rep_name = 'Selenium_test'
     verif_for_delete_rep = 'your login/Selenium_test' # add login
--- test/test_create_rep_in_github.py
+++ test/test_create_rep_in_github.py
@@ -1,5 +1,6 @@
 from pages.main_page import MainPage
 from pages.base_page import BasePage
+from data.user_data import User
 
 
 class TestGitHub:
@@ -10,4 +11,4 @@ class TestGitHub:
         main_page.auth_on_the_github()
         main_page.create_new_rep()
         message = main_page.delete_new_rep()
-        assert 'Your repository "luufqa/Selenium_test" was successfully deleted.' in message
+        assert f'Your repository "{User.verif_for_delete_rep}" was successfully deleted.' in message
